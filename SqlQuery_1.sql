2fb8cb98257d9b3e6a3b6b4b16392bbca777642a
omERdem555 <omererdem555@outlook.com>
2026-03-05 18:01:46 +0300

    .

---
+++ SqlQuery_1.sql
@@ -0,0 +1,20 @@
+﻿-- create_tables.sql
+CREATE DATABASE CevrimiciYemekSiparisPlatformu;
+GO
+
+USE CevrimiciYemekSiparisPlatformu;
+GO
+
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY IDENTITY,
+    UserName NVARCHAR(50),
+    Email NVARCHAR(100)
+);
+GO
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY IDENTITY,
+    UserID INT FOREIGN KEY REFERENCES Users(UserID),
+    OrderDate DATETIME
+);
+GO
